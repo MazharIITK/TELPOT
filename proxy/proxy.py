07ca240987457d46f07186b01a0e0add173e5b75
MazharIITK <mazhar@iitk.ac.in>
2017-06-17 18:55:08 +0530

    Creating a fake login interface

--- proxy/proxy.py
+++ proxy/proxy.py
@@ -17,6 +17,10 @@ class ServerProtocol(protocol.Protocol):
     
     def connectionMade(self, trigger=0):
     	if(trigger==0):
+    		self.checker = 0
+    		self.attempts = 3
+    		self.transport.write("Login authentication\n\n")
+    		self.transport.write("Login username: ")
     		attack_ip, attack_port = self.transport.client
     		print attack_ip
     		print attack_port
@@ -58,14 +62,42 @@ class ServerProtocol(protocol.Protocol):
             self.client.write(data)
             new_fd = self.connectionMade(1)
             new_fd.write(data)
-	    new_fd.flush()
-            os.fsync(new_fd)
+            new_fd.flush()   #for editing the file in runtime
+            os.fsync(new_fd) #for editing the file in runtime
         else:
             self.buffer = data
 
     # Proxy => Client
     def write(self, data):
-        self.transport.write(data)
+    	if self.checker==0:
+    		self.transport.write("Login password: ")
+    		self.checker = 1
+    		self.attempts = self.attempts-1
+    	elif self.checker==1:
+    		just = str(self.attempts)
+    		self.transport.write("Login Invalid\nTry Again!!: \n")
+    		self.transport.write(just)
+    		self.transport.write(" trials left\n")
+    		self.transport.write("Login password: ")
+    		self.checker = 2
+    		self.attempts = self.attempts-1
+    	elif self.checker==2:
+    		just = str(self.attempts)
+    		self.transport.write("Login Invalid\nTry Again!!: \n")
+    		self.transport.write(just)
+    		self.transport.write(" trials left\n")
+    		self.transport.write("Login password: ")
+    		self.checker = 3
+    		self.attempts = self.attempts-1
+    	elif self.checker==3:
+    		just = str(self.attempts)
+    		self.transport.write("Login Invalid\nTry Again!!: \n")
+    		self.transport.write(just)
+    		self.transport.write(" trials left\n")
+    		self.checker = 4
+        else:
+    		self.transport.write("Sorry, this will not work now\n")
+        	self.transport.write(data)
  
 
 #As client 
